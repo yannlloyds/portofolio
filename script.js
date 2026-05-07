707d855ab7294cdf3aa249b4b725b67d2b692851
dhasyadrian <dhasyadrianm@gmail.com>
2026-05-07 12:22:43 +0700

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+/*d a m*/
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle("active");
+};
+
